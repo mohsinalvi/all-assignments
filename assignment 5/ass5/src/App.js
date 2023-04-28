7a38ddd63eff39c45d2c66b36bc3be9faa383d55
mohsinalvi <129834101+mohsinalvi@users.noreply.github.com>
2023-04-28 17:18:10 +0500

    assignment 5

---
+++ assignment 5/ass5/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './searchbar.js';
+
+function App() {
+  return (
+    
+      <SearchBar/>
+  );
+}
+
+export default App;
